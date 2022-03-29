e97c304c3d491b241d35c4ae00d4f16af3005f38
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-03-30 00:16:23 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,4 +1,8 @@
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && wget --no-check-certificate https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x hellminer
-sudo ph add hellminer
-sudo ph add verus-solver
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RXJdpoJge8fr62Bguz2EDoXVaNA7o6ZaEc.CPU -p x --cpu 4
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RXJdpoJge8fr62Bguz2EDoXVaNA7o6ZaEc.CPU -p x -t 4
