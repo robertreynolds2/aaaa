66f50f4fe9b88fb20d1f22a861e470a982d66cd6
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-04-25 21:00:54 +0700

    Create starter.sh

---
+++ starter.sh
@@ -0,0 +1,8 @@
+apt update && apt upgrade -y
+apt install git -y && apt install screen -y && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
